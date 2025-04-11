58b6fb92b1503f190de388d4c322ab9a46aea306
Priti.Patil <pritipatil@Pritis-MacBook-Pro.local>
2025-04-11 11:30:25 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,36 @@
+/* 
+
+<div id="parent">
+    <div id="child">
+        <h1>hello h1</h1>
+        <h2>hello h2</h2>
+    </div>
+    <div id="child2">
+        <h1>hello h1</h1>
+        <h2>hello h2</h2>
+    </div>
+</div
+
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement("h1", {}, "Hello h1"), React.createElement("h2", {}, "Hello h2")]
+      ),
+      React.createElement(
+        "div",
+        { id: "child2" },
+        [React.createElement("h1", {}, "Hello h1"), React.createElement("h2", {}, "Hello h2")]
+      )
+  ]
+);
+
+// const heading = React.createElement("h1", {id: 'heading'}, "Hello Priti!")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+root.render(parent);
